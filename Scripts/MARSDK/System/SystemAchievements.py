b14cbb526a67cd3b08e3250d023b0cfe3c2d09ab
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-04-04 19:48:39 +0200

    add f2p ids

--- Scripts/MARSDK/System/SystemAchievements.py
+++ Scripts/MARSDK/System/SystemAchievements.py
@@ -1,7 +1,7 @@
 from Foundation.Notificator import Notificator
 from Foundation.PolicyManager import PolicyManager
 from Foundation.TaskManager import TaskManager
-from Foundation.Utils import getCurrentPublisher
+from Foundation.Utils import getCurrentPublisher, getCurrentBusinessModel
 from HOPA.System.SystemAchievements import SystemAchievements as SystemAchievementsBase
 from MARSDK.MarUtils import MarUtils
 from Notification import Notification
@@ -9,29 +9,56 @@ from Notification import Notification
 
 class SystemAchievements(SystemAchievementsBase):
 
+    s_mar_achieve_ids = {
+        "BatAll": {
+            "Free": "Bat_All_F2P",
+            "Premium": "Bat_All"
+        },
+        "DiaryAll": {
+            "Free": "Diary_All_F2P",
+            "Premium": "Diary_All"
+        }
+    }
+
     # observers
 
-    def __onCollectiblesComplete(self):  ### Catch all the bats
+    def __onCollectiblesComplete(self, business_model):   # Catch all the bats
         policy_progress = PolicyManager.getPolicy("ExternalAchieveProgress", "PolicyDummy")
+
         if MarUtils.isMartianIOS():
-            TaskManager.runAlias(policy_progress, None, AchieveId="Bat_All", Complete=True)
+            AchieveId = self.s_mar_achieve_ids["BatAll"].get(business_model)
+
+            if AchieveId is None:
+                Trace.log("System", 0, "__onCollectiblesComplete: business model is None!!!!!")
+                return True
+
+            TaskManager.runAlias(policy_progress, None, AchieveId=AchieveId, Complete=True)
+
         elif getCurrentPublisher() == "TellTale" and _ANDROID:
             Notification.notify(Notificator.onAchievementExternalUnlocked, "CgkI3q2FnaUHEAIQHg")
 
         return True
 
-    def __onJournalAllPagesFound(self):  ### Complete all logs
+    def __onJournalAllPagesFound(self, business_model):   # Complete all logs
         policy_progress = PolicyManager.getPolicy("ExternalAchieveProgress", "PolicyDummy")
+
         if MarUtils.isMartianIOS():
-            TaskManager.runAlias(policy_progress, None, AchieveId="Diary_All", Complete=True)
+            AchieveId = self.s_mar_achieve_ids["DiaryAll"].get(business_model)
+
+            if AchieveId is None:
+                Trace.log("System", 0, "__onCollectiblesComplete: business model is None!!!!!")
+                return True
+
+            TaskManager.runAlias(policy_progress, None, AchieveId=AchieveId, Complete=True)
         return True
 
     # system things
 
     def _onRun(self):
         super(SystemAchievements, self)._onRun()
+        current_business_model = getCurrentBusinessModel()
 
-        self.addObserver(Notificator.onCollectiblesComplete, self.__onCollectiblesComplete)
-        self.addObserver(Notificator.onJournalAllPagesFound, self.__onJournalAllPagesFound)
+        self.addObserver(Notificator.onCollectiblesComplete, self.__onCollectiblesComplete, current_business_model)
+        self.addObserver(Notificator.onJournalAllPagesFound, self.__onJournalAllPagesFound, current_business_model)
 
         return True
