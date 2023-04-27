59d7af112af6930bd109626e4ba6e0dc8e99ff56
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-04-27 10:08:19 +0200

    upd _getTimestamp

--- Scripts/MARSDK/System/SystemEnergy.py
+++ Scripts/MARSDK/System/SystemEnergy.py
@@ -4,8 +4,7 @@ from MARSDK.System.SystemMARSDK import SystemMARSDK
 
 class SystemEnergy(SystemEnergyBase):
 
-    @staticmethod
-    def _getTimestamp():
+    def _getTimestamp(self):
         if SystemMARSDK.hasActiveSDK():
             return SystemMARSDK.getNetworkTimeMs()
-        return SystemEnergyBase._getTimestamp()
+        return super(SystemEnergy, self)._getTimestamp()
