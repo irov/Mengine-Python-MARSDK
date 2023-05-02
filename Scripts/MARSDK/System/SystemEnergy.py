896790d8317d6724408bb3c47021cbe5ff2a86fc
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-05-02 13:40:43 +0200

    always use mar time if all params are for martian game

--- Scripts/MARSDK/System/SystemEnergy.py
+++ Scripts/MARSDK/System/SystemEnergy.py
@@ -1,10 +1,10 @@
 from HOPA.System.SystemEnergy import SystemEnergy as SystemEnergyBase
 from MARSDK.System.SystemMARSDK import SystemMARSDK
+from MARSDK.MarUtils import MarUtils
 
 
 class SystemEnergy(SystemEnergyBase):
 
-    def _getTimestamp(self):
-        if SystemMARSDK.hasActiveSDK():
+    if MarUtils.isMartianAndroid() is True:
+        def _getTimestamp(self):
             return SystemMARSDK.getNetworkTime()
-        return super(SystemEnergy, self)._getTimestamp()
