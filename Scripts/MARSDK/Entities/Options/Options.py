781b4a54cf6c640f1e3127120dadc507d51d0e23
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2023-06-28 21:08:03 +0300

    Added some logic to open\close About, rework sockets and movies

--- Scripts/MARSDK/Entities/Options/Options.py
+++ Scripts/MARSDK/Entities/Options/Options.py
@@ -9,6 +9,10 @@ class Options(OptionsBase):
 
     def _onActivate(self):
         super(Options, self)._onActivate()
+        self.clickMartianURLs()
+        self.clickMartianAbout()
+
+    def clickMartianURLs(self):
         if GroupManager.hasObject("Options", "Movie2_chinese_urls") is False:
             return
         if MarUtils.isMartianIOS() is False:
@@ -22,13 +26,32 @@ class Options(OptionsBase):
             3: unicode(MarParamsManager.getData("url_3"), "utf-8"),
         }
 
-        with TaskManager.createTaskChain(Name="Menu_Options_Chinese_URLs", Repeat=True) as tc:
+        with TaskManager.createTaskChain(Name="Menu_Options_Martian_URLs", Repeat=True) as tc:
             for (i, url), race in tc.addRaceTaskList(urls.items()):
                 race.addTask("TaskMovie2SocketClick", SocketName="url_%s" % i, Movie2=chinese_urls_movie)
                 race.addFunction(Mengine.openUrlInDefaultBrowser, url)
 
+    def clickMartianAbout(self):
+        if GroupManager.hasObject("Options", "Movie2Button_About") is False:
+            return
+        if MarUtils.isMartianIOS() is False:
+            return
+
+        btn_about = GroupManager.getObject("Options", "Movie2Button_About")
+        btn_about.setParam("Enable", True)
+
+        with TaskManager.createTaskChain(Name="Menu_Options_Martian_About", Repeat=True) as tc:
+            tc.addTask("TaskMovie2ButtonClick", Movie2Button=btn_about)
+            tc.addScope(self.SceneEffect, "Options", "Movie2_Close")
+            tc.addTask("TaskSceneLayerGroupEnable", LayerName="About", Value=True)
+            tc.addScope(self.SceneEffect, "About", "Movie2_Open")
+            tc.addTask("TaskSceneLayerGroupEnable", LayerName="Options", Value=False)
+
     def _onDeactivate(self):
         super(Options, self)._onDeactivate()
 
-        if TaskManager.existTaskChain("Menu_Options_Chinese_URLs"):
-            TaskManager.cancelTaskChain("Menu_Options_Chinese_URLs")
+        if TaskManager.existTaskChain("Menu_Options_Martian_URLs"):
+            TaskManager.cancelTaskChain("Menu_Options_Martian_URLs")
+
+        if TaskManager.existTaskChain("Menu_Options_Martian_About"):
+            TaskManager.cancelTaskChain("Menu_Options_Martian_About")
