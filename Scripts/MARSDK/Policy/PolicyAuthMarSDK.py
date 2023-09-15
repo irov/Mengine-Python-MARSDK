0565324f37ba07561726beca747f04a5f9210747
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-09-15 15:41:37 +0300

    fix dummy marsdk auth with policy

--- Scripts/MARSDK/Policy/PolicyAuthMarSDK.py
+++ Scripts/MARSDK/Policy/PolicyAuthMarSDK.py
@@ -25,12 +25,18 @@ class PolicyAuthMarSDK(TaskAlias):
                 response.addScope(self.scopeWaitLogin)
                 request.addScope(self.scopeCallLogin)
 
+    def _scopeDummyAuth(self, source):
+        with source.addIfTask(SystemMARSDK.isLogged) as (true, false):
+            true.addFunction(Trace.msg, "<SystemMARSDK> [MarSDK not inited] dummy already logged in")
+
+            false.addFunction(Trace.msg, "<SystemMARSDK> [MarSDK not inited] dummy login success")
+            false.addFunction(SystemMARSDK._cbLoginSuccess)
+
     def _onGenerate(self, source):
         if SystemMARSDK.hasActiveSDK() is True:
             source.addScope(self._scopeAuthProcessing)
         elif MarUtils.isMartianTouchpadDevice() and _DEVELOPMENT is True:
-            source.addPrint("<SystemMARSDK> [MarSDK not inited] dummy auth moment...")
-            source.addNotify(Notificator.onUserLoggedIn)
+            source.addScope(self._scopeDummyAuth)
         else:
             Trace.msg_err("<SystemMARSDK> auth error: no active sdk !!!")
             source.addDummy()
