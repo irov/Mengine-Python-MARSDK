676d94549fa8461150279083f6715b87c8c9be3a
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-10-20 13:50:52 +0300

    add debug code test

--- Scripts/MARSDK/Entities/GiftExchange/GiftExchange.py
+++ Scripts/MARSDK/Entities/GiftExchange/GiftExchange.py
@@ -1,10 +1,11 @@
 ﻿from HOPA.Entities.GiftExchange.GiftExchange import GiftExchange as GiftExchangeBase
 from HOPA.Entities.GiftExchange.GiftExchange import _Log
 from MARSDK.System.SystemMARSDK import SystemMARSDK
+from MARSDK.MarUtils import MarUtils
 
 
 class GiftExchange(GiftExchangeBase):
-    if SystemMARSDK.sdk_init is True:
+    if SystemMARSDK.isSDKInited():
         def _getFromClipboard(self):
             SystemMARSDK.getFromClipboard()
             clipboard = SystemMARSDK.clipboard
@@ -22,3 +23,21 @@ class GiftExchange(GiftExchangeBase):
 
             _Log(msg)
             SystemMARSDK.redeemCode(code)
+
+    elif MarUtils.isMartianAndroid() and _DEVELOPMENT is True:
+        def _sendRequest(self):
+            code = self._getInputCode()
+
+            if code == "timeout":
+                return
+
+            type_of_rewards = ["unlock", "golds", "energy", "guide"]
+            _Log("DUMMY Redeem code {!r}...".format(code))
+
+            if code not in type_of_rewards:
+                _Log("DUMMY unknown reward code {!r}".format(code), err=True)
+                SystemMARSDK._cbRedeemResultSuccess(0, code, "0")
+                return
+
+            reward_size = 1 + Mengine.rand(50)
+            SystemMARSDK._cbRedeemResultSuccess(reward_size, code, "1")
