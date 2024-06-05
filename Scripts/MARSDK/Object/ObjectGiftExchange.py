8538d089a27c3f63b9bad142dc5f70c5bd0af034
eclipse7723 <eclipse7723@gmail.com>
2024-06-05 20:40:05 +0300

    add in-game item promocodes

--- Scripts/MARSDK/Object/ObjectGiftExchange.py
+++ Scripts/MARSDK/Object/ObjectGiftExchange.py
@@ -1,5 +1,34 @@
 ﻿from Foundation.Object.DemonObject import DemonObject
+from Foundation.TaskManager import TaskManager
+
+FLOW_TC_NAME = "GiftExchange_Flow"
 
 
 class ObjectGiftExchange(DemonObject):
-    pass
+
+    def runTaskChain(self, button, fade_value, fade_time, fade_group="FadeUI"):
+        if TaskManager.existTaskChain(FLOW_TC_NAME) is True:
+            Trace.log("Object", 0, "GiftExchange: {!r} already exist".format(FLOW_TC_NAME))
+            return
+
+        with TaskManager.createTaskChain(Name=FLOW_TC_NAME, Global=True, Repeat=True) as tc:
+            tc.addTask("TaskMovie2ButtonClick", Movie2Button=button)
+
+            with tc.addParallelTask(2) as (tc_fade, tc_layer):
+                tc_fade.addTask('AliasFadeIn', FadeGroupName=fade_group, To=fade_value, Time=fade_time)
+                tc_layer.addTask("TaskSceneLayerGroupEnable", LayerName="GiftExchange", Value=True)
+
+            with tc.addRepeatTask() as (repeat, until):
+                repeat.addScope(self.entity.scopeOpenPlate)
+                repeat.addScope(self.entity.scopeRun)
+
+                until.addScope(self.entity.scopeClickQuit)
+                until.addScope(self.entity.scopeClosePlate)
+
+            with tc.addParallelTask(2) as (tc_fade, tc_layer):
+                tc_fade.addTask("AliasFadeOut", FadeGroupName=fade_group, From=fade_value, Time=fade_time)
+                tc_layer.addTask("TaskSceneLayerGroupEnable", LayerName="GiftExchange", Value=False)
+
+    def cancelTaskChain(self):
+        if TaskManager.existTaskChain(FLOW_TC_NAME) is True:
+            TaskManager.cancelTaskChain(FLOW_TC_NAME)
--- Scripts/MARSDK/System/SystemMonetization.py
+++ Scripts/MARSDK/System/SystemMonetization.py
@@ -13,35 +13,10 @@ class SystemMonetization(SystemMonetizationBase):
 
     if MarUtils.isMartianAndroid() is True:
         @staticmethod
-        def _onGiftExchangeRedeemResult(reward_type, reward_amount):
-            _Log("onGiftExchangeRedeemResult - {} {}".format(reward_type, reward_amount))
-
-            if reward_type is None:
-                return False
-
-            def _callPaySuccess(prod_id):
-                Notification.notify(Notificator.onPaySuccess, prod_id)
-
-            rewards = {
-                "golds": [SystemMonetization.addGold, reward_amount],
-                "energy": [SystemMonetization.addEnergy, reward_amount],
-                "MysteryChapter": [SystemMonetization.unlockChapter, "Bonus"],
-            }
-
-            guide_product_id = MonetizationManager.getGeneralSetting("GuidesProductID")
-            if MonetizationManager.hasProductInfo(guide_product_id):
-                guide_product = MonetizationManager.getProductInfo(guide_product_id)
-                rewards["guide"] = [_callPaySuccess, guide_product.id]
-
-            if reward_type not in rewards:
-                Trace.log("System", 0, "SystemMonetization (MarSDK) reward_type {!r} is unknown".format(reward_type))
-                return False
-
-            send_reward, arg = rewards[reward_type]
-            send_reward(arg)
-
-            return False
-
-        @staticmethod
         def shouldAcceptPrice():
             return True
+
+        def _getPossibleGiftExchangeRewards(self, reward_amount):
+            rewards = super(SystemMonetization, self)._getPossibleGiftExchangeRewards(reward_amount)
+            rewards["MysteryChapter"] = rewards.pop("chapter")  # rename key `chapter` to `MysteryChapter`
+            return rewards
